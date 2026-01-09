9974ba4bbbc135c5ef9e74b6f741f6c7278151dd
MrAqil123 <Mraqil1236@gmail.com>
2026-01-09 05:48:36 +0430

    new update 0.0.3

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,33 +1,19 @@
 export const getImgPath = (path: string): string => {
-  const basePath = import.meta.env.NEXT_PUBLIC_BASE_PATH || "";
-  
-  if (!basePath) {
-    return path;  
-  }
-  
-  if (path.startsWith(basePath)) {
-    return path; 
-  }
-  
+  const basePath = import.meta.env.VITE_BASE_PATH || "";
+
+  if (!basePath) return path;
+
+  if (path.startsWith(basePath)) return path;
+
   return `${basePath}${path}`;
 };
 
 export const getDataPath = (path: string): string => {
-  if (typeof window !== "undefined") {
-    const basePath = window.location.pathname.split("/")[1] || "";
-    if (basePath && path.startsWith("/")) {
-      return `/${basePath}${path}`;
-    }
-  }
-  
-  const basePath = import.meta.env.NEXT_PUBLIC_BASE_PATH || "";
-  if (!basePath) {
-    return path;
-  }
-  
-  if (path.startsWith(basePath)) {
-    return path;
-  }
-  
+  const basePath = import.meta.env.VITE_BASE_PATH || "";
+
+  if (!basePath) return path;
+
+  if (path.startsWith(basePath)) return path;
+
   return `${basePath}${path}`;
 };
